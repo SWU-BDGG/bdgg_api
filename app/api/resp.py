3b8ae653fd02a5347705aa8f415b63449ac5c0e3
chick_0 <chick_0@ch1ck.xyz>
2021-11-09 23:43:55 +0900

    add api version

--- app/api/resp.py
+++ app/api/resp.py
@@ -2,14 +2,15 @@
 from flask import jsonify
 
 
-def on_error(message: str, code: str = "undefined", data: dict = None):
+def on_error(message: str, code: str = "undefined", data: dict = None, api_version: str = "undefined"):
     return jsonify({
         "status": "error",
         "error": {
             "code": code,
             "message": message,
             "data": data if data is not None else {}
-        }
+        },
+        "api_version": api_version
     })
 
 
