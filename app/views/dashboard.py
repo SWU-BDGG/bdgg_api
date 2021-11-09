80ba228b356d39777edf3ab9bfb5f4b55f94483f
chick_0 <chick_0@ch1ck.xyz>
2021-11-10 00:37:20 +0900

    파일 대시보드 & 세부정보 조회

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,10 +1,12 @@
 
 from flask import Blueprint
+from flask import request
 from flask import redirect
 from flask import url_for
 from flask import render_template
 
 from app.check import check_login
+from app.models import File
 from app.models import Login
 
 
@@ -37,4 +39,42 @@ def index():
 
 @bp.get("/file")
 def file():
-    return "TODO"
+    user = check_login(bool_instead=True)
+
+    if isinstance(user, bool):
+        return redirect(url_for("user.login"))
+
+    try:
+        page = int(request.args.get("page", "1"))
+
+        if page <= 0:
+            page = 1
+    except ValueError:
+        page = 1
+
+    files = File.query.filter_by(
+        owner=user.id
+    ).order_by(
+        File.date.desc()
+    ).paginate(page)
+
+    pages = []
+
+    if files.page < 3:
+        for i in range(1, files.pages + 1):
+            if len(pages) == 5:
+                break
+
+            pages.append(i)
+    else:
+        for i in range(files.page - 2, files.pages + 1):
+            if len(pages) == 5:
+                break
+
+            pages.append(i)
+
+    return render_template(
+        "user/file.html",
+        files=files,
+        pages=pages
+    )
--- app/views/file.py
+++ app/views/file.py
@@ -1,5 +1,11 @@
 
 from flask import Blueprint
+from flask import Response
+from flask import render_template
+
+from app.models import File
+from app.models import User
+from app.custom_error import FileIsDeletedOrNotUploaded
 
 
 bp = Blueprint(
@@ -17,3 +23,42 @@ def index():
 @bp.get("/search")
 def search():
     return "file.search"
+
+
+@bp.get("/detail/<string:file_id>")
+def detail(file_id: str):
+    file = File.query.filter_by(
+        uuid=file_id
+    ).first()
+
+    if file is None:
+        raise FileIsDeletedOrNotUploaded
+
+    upload_user = User.query.filter_by(
+        id=file.owner
+    ).first()
+
+    return render_template(
+        "file/detail.html",
+        file=file,
+        upload_user=upload_user
+    )
+
+
+@bp.get("/detail/<string:file_id>/checksum.txt")
+def checksum(file_id: str):
+    file = File.query.filter_by(
+        uuid=file_id
+    ).first()
+
+    if file is None:
+        raise FileIsDeletedOrNotUploaded
+
+    return Response(
+        mimetype="text/plain",
+        response="\n".join([
+            file.name,
+            f"md5sum {file.md5}",
+            f"sha256sum {file.sha256}",
+        ])
+    )
